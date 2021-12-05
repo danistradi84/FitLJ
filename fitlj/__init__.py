8996704812a8839a139d9eb517a20410526420f4
danistradi84 <danistradi@gmail.com>
2021-12-05 15:58:34 +0100

    First commit

---
+++ fitlj/__init__.py
@@ -0,0 +1,9 @@
+"""Documentation about fitlj"""
+import logging
+
+
+logging.getLogger(__name__).addHandler(logging.NullHandler())
+
+__author__ = "Daniele Stradi"
+__email__ = "danistradi@gmail.com"
+__version__ = "0.1.0"
